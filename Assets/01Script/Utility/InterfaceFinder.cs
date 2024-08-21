2b8f8c68df6b5c04d9205bbffb589aff1e7f869f
SquallLab <bluelemon87@naver.com>
2024-08-21 19:51:42 +0900

    기반작업
    스프라이트 임포트,
    게임매니저
    해상도매니저
    interfaceFinder 제작.

---
+++ Assets/01Script/Utility/InterfaceFinder.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 순수 가상함수로만 이루어진 추상클래스 
+
+
+
+public class InterfaceFinder : MonoBehaviour
+{
+    //public static List<T> FinObjectsOfInterface<T>() where T : class
+    //{
+    //    MonoBehaviour[] allObjects = FindObjectsByType<MonoBehaviour>();
+    //    List<T> interfaceObjects = new List<T>();
+
+    //    foreach(var obj in allObjects)
+    //    {
+    //        if(obj is T allObjects)
+    //        {
+    //            interfaceObjects.Add(obj);
+    //        }
+    //    }
+    //    return interfaceObjects;
+    //}
+}
