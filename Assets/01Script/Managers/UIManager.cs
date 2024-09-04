f25a494cd07a51f002240d5a943bd5b55b72dc8e
SquallLab <bluelemon87@naver.com>
2024-09-04 21:54:38 +0900

    UI 연동작업

--- Assets/01Script/Managers/UIManager.cs
+++ Assets/01Script/Managers/UIManager.cs
@@ -1,10 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class UIManager : MonoBehaviour
 {
     Transform canvasTrans;
+    TextMeshProUGUI scoreText;
+    TextMeshProUGUI jamText;
+    TextMeshProUGUI bombText;
+    [SerializeField]
+    Image[] heartImg;
+
+    TextMeshProUGUI ScoreText
+    {
+        get
+        {
+            if (scoreText == null)
+            {
+                scoreText = canvasTrans.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+            }
+            return scoreText;
+        }
+    }
+
+    TextMeshProUGUI JamText
+    {
+        get
+        {
+            if (jamText == null)
+            {
+                jamText = canvasTrans.Find("JamText").GetComponent<TextMeshProUGUI>();
+            }
+            return jamText;
+        }
+    }
+    TextMeshProUGUI BombText
+    {
+        get
+        {
+            if (bombText == null)
+            {
+                bombText = canvasTrans.Find("BombText").GetComponent<TextMeshProUGUI>();
+            }
+            return bombText;
+        }
+    }
 
 
     private void Awake()
@@ -12,7 +53,37 @@ public class UIManager : MonoBehaviour
         GameObject obj = GameObject.Find("Canvas");
         canvasTrans = obj.GetComponent<Transform>();
 
-        canvasTrans.Find("ScoreText");
+    
+    }
 
+    private void OnEnable()
+    {
+        //이벤트 구독 
+
+        ScoreManager.OnChangeScore += UpdateScoreText;
+        ScoreManager.OnChangeBomb += UpdateBombText;
+        ScoreManager.OnChangeJamCount += UpdateJamText;
+    }
+
+    private void OnDisable()
+    {
+        ScoreManager.OnChangeScore -= UpdateScoreText;
+        ScoreManager.OnChangeBomb -= UpdateBombText;
+        ScoreManager.OnChangeJamCount -= UpdateJamText;
+    }
+
+    private void UpdateScoreText(int score)
+    {
+        ScoreText.text = score.ToString();
+    }
+
+    private void UpdateBombText(int score)
+    {
+        BombText.text = "X : " + score.ToString();
+    }
+
+    private void UpdateJamText(int score)
+    {
+        JamText.text = score.ToString();
     }
 }
